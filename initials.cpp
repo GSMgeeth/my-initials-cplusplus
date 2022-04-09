b271412e7a6b37c6b015d8cabf43b6c55b0443a9
Geeth Sandaru Madhushan <geeth.gsm@gmail.com>
2022-04-10 01:53:20 +0530

    cout initials.

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+/**
+* I am a computer science enthusiast...!
+**/
+
+#include <iostream>
+
+int main() {
+  std::cout << "  A    GGG    A  " << std::endl;
+  std::cout << " A A  G   G  A A " << std::endl;
+  std::cout << "A   A G     A   A" << std::endl;
+  std::cout << "AAAAA GGGGG AAAAA" << std::endl;
+  std::cout << "A   A G   G A   A" << std::endl;
+  std::cout << "A   A G   G A   A" << std::endl;
+  std::cout << "A   A  GGG  A   A" << std::endl;
+}
